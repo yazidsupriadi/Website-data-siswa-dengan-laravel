867064ee902b9b1bb4767da4cf6ffeac5b26a1bd
yazidsupriadi14 <yazidsupriadi14@gmail.com>
2019-03-31 20:53:54 +0700

    sekolah

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiswaController extends Controller
+{
+    //
+   public  function index(Request $request)
+    {	
+    	if ($request->has('cari')) {
+    		$data_siswa = \App\Siswa::where('nama_depan','LIKE','%'.$request->cari.'%')->get();	
+    		
+    	}else {
+    		
+    	$data_siswa = \App\Siswa::all();		
+    	}
+    	return view('siswa.index',['data_siswa'=>$data_siswa]);
+    }
+
+   public  function create(Request $request)
+    {	
+    	\App\Siswa::create($request->all());
+    	return redirect('/siswa')->with('success','Data masuk!');
+    }
+
+   public  function edit($id)
+    {	
+    	$siswa = \App\Siswa::find($id);
+    	return view('siswa/edit',['siswa' =>$siswa]);
+    }
+
+   public  function update(Request $request,$id)
+    {	
+    	$siswa = \App\Siswa::find($id);
+    	$siswa->update($request->all());
+		return redirect('/siswa')->with('success','Data Di update!');
+        	
+    }
+
+   public  function delete($id)
+    {	
+    	$siswa = \App\Siswa::find($id);
+    	$siswa->delete($siswa);
+    	return redirect('/siswa')->with('success','Data DiHapus!');
+        	
+    }
+
+}
---
+++ resources/views/siswa/edit.blade.php
@@ -0,0 +1,107 @@
+@extends('layout.master')
+@section('content')
+	
+	<div class="main">
+		<div class="main-content">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-12">
+						
+		<h1> edit data siswa</h1>
+		<div class="row">
+			<div class="col-lg-12">
+				
+		      <form action="/siswa/{{$siswa->id}}/update" method="post">
+		      	{{csrf_field()}}
+			  <div class="form-group">
+			    <label for="exampleInputEmail1">Nama Depan</label>
+			    <input type="text" name='nama_depan' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Depan" value="{{$siswa->nama_depan}}">
+			  </div>
+
+			  <div class="form-group">
+			    <label for="exampleInputEmail1">Nama Belakang</label>
+			    <input name='nama_belakang' type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama belakang" value="{{$siswa->nama_belakang}}">
+			  </div>
+			   <div class="form-group">
+			    <label for="exampleFormControlSelect1">Jenis Kelamin</label>
+			    <select name='jenis_kelamin' class="form-control" id="exampleFormControlSelect1" >
+			      <option value="L" @if($siswa->jenis_kelamin == 'L') selected @endif>Laki Laki</option>
+			      <option value="P" @if($siswa->jenis_kelamin == 'P') selected @endif>Perempuan</option>
+			    </select>
+			  </div>
+
+			  <div class="form-group">
+			    <label for="exampleInputEmail1">Agama</label>
+			    <input type="text" name='agama' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Agama" value="{{$siswa->agama}}">
+			  </div>
+			    <div class="mb-3">
+					<label for="validationTextarea">Alamat</label>
+					<textarea name='alamat' class="form-control " id="validationTextarea" placeholder="Alamat"  >{{$siswa->alamat}}</textarea>
+					
+				</div>
+			  <div class="form-group form-check">
+			    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+			    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+			  </div>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+			  <button type="submit" class="btn btn-warning">Update</button>
+			</form>
+	
+			</div>
+		</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</div>			
+@stop
+@section('content1')
+		<h1> edit data siswa</h1>
+		<div class="row">
+			<div class="col-lg-12">
+				
+		      <form action="/siswa/{{$siswa->id}}/update" method="post">
+		      	{{csrf_field()}}
+			  <div class="form-group">
+			    <label for="exampleInputEmail1">Nama Depan</label>
+			    <input type="text" name='nama_depan' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Depan" value="{{$siswa->nama_depan}}">
+			  </div>
+
+			  <div class="form-group">
+			    <label for="exampleInputEmail1">Nama Belakang</label>
+			    <input name='nama_belakang' type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama belakang" value="{{$siswa->nama_belakang}}">
+			  </div>
+			   <div class="form-group">
+			    <label for="exampleFormControlSelect1">Jenis Kelamin</label>
+			    <select name='jenis_kelamin' class="form-control" id="exampleFormControlSelect1" >
+			      <option value="L" @if($siswa->jenis_kelamin == 'L') selected @endif>Laki Laki</option>
+			      <option value="P" @if($siswa->jenis_kelamin == 'P') selected @endif>Perempuan</option>
+			    </select>
+			  </div>
+
+			  <div class="form-group">
+			    <label for="exampleInputEmail1">Agama</label>
+			    <input type="text" name='agama' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Agama" value="{{$siswa->agama}}">
+			  </div>
+			    <div class="mb-3">
+					<label for="validationTextarea">Alamat</label>
+					<textarea name='alamat' class="form-control " id="validationTextarea" placeholder="Alamat"  >{{$siswa->alamat}}</textarea>
+					
+				</div>
+			  <div class="form-group form-check">
+			    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+			    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+			  </div>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+			  <button type="submit" class="btn btn-warning">Update</button>
+			</form>
+	
+			</div>
+		</div>
+@endsection
